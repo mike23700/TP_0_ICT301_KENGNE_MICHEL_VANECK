03c90e0b7cb86b800469319de87095aa68931f06
mike23700 <kengnem070@gmail.com>
2025-12-16 23:36:47 +0100

    creation des differentes classes de Book apres refactoring

---
+++ SRP/BookSaver.java
@@ -0,0 +1,10 @@
+public class BookSaver {
+    public void saveToDatabase(BookSRP book) {
+        System.out.println("Sauvegarde de '" + book.getTitle() + "' en base de données...");
+    }
+
+    //On peut ajouter d'autres façons de sauvegarder
+    public void saveToFile(BookSRP book, String filename) {
+        System.out.println("\nSauvegarde de '" + book.getTitle() + "' dans " + filename); 
+    }        
+}
