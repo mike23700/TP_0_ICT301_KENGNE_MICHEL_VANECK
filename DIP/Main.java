b64339de10d93ca5c3f9d536c09bca0bf44c3267
mike23700 <kengnem070@gmail.com>
2025-12-24 00:27:39 +0100

    DIP avant refactoring

---
+++ DIP/Main.java
@@ -0,0 +1,8 @@
+package ict301.solid.dip.old;
+
+public class Main {
+    public static void main(String[] args) {
+        OrderProcessor order = new OrderProcessor();
+        order.processOrder("'Données à sauvegarder'");
+    }
+}
